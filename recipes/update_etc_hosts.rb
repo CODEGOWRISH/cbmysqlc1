d43cd87fa0f2062045377a7a5c4d1ac69e61dde5
gowrishdba <gowrishdba@yahoo.com>
2015-11-20 12:41:05 +0530

    "Fixed etc hosts lines. yet to do /usr/mysql-cluster piece and ndb/mgmd startups" 	modified:   .gitignore 	modified:   Vagrantfile 	modified:   recipes/update_etc_hosts.rb

--- recipes/update_etc_hosts.rb
+++ recipes/update_etc_hosts.rb
@@ -14,8 +14,11 @@ bash 'update etc hosts file' do
   code <<-EOH
 
   cp '#{hostsFile}' '#{hostsFile}.orig'
-  echo #{node[:node1][:ip]}  #{node[:node1][:hostName]}  > '#{hostsFile}'
+
+  echo '127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4' > '#{hostsFile}'
+  echo #{node[:node1][:ip]}  #{node[:node1][:hostName]} >> '#{hostsFile}'
   echo #{node[:node2][:ip]}  #{node[:node2][:hostName]} >> '#{hostsFile}'
+  echo '::1         localhost localhost.localdomain localhost6 localhost6.localdomain6' >> '#{hostsFile}'
 
   EOH
 
