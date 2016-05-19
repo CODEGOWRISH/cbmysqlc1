202e1afcc7e1a72d3f4a0115bec1a2b26b1f51d0
Coder <coderdba@users.noreply.github.com>
2016-05-19 23:11:00 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,4 @@
 #                                                                                         
-# Author: Gowrish.Mallipattana                                                            
-#                                                                                         
 # Attributes file default.rb for mysql installation                                      
 #                                                                                         
 #                                                                                         
